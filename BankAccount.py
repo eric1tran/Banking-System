6d58a84fe788c2a8f82d963c4777c6d3ec5eda8a
Eric Tran <eric1tran@gmail.com>
2018-03-28 23:18:28 -0500

    Minor improvements

--- BankAccount.py
+++ BankAccount.py
@@ -27,26 +27,28 @@ class BankAccount:
     def withdraw(self, amount):
         print ("Withdrawing ${:.2f}...".format(amount))
         if amount > self.balance:
-            print ("Insufficient funds")
+            print ("Insufficient funds\n")
         else:
             self.balance -= amount
             self.print_balance()
 
 
 def CreateAccount(user_name, initial_deposit):
+    """
+    :param user_name: User name to create a Bank Account for
+    :param initial_deposit: The initial amount user wants to deposit into account
+    :return: None
+    """
     bank_acc = BankAccount(user_name, initial_deposit)
     accounts[user_name] = bank_acc
-    print ("\n")
-    print ("Thank you! Here is your Bank info: ")
+
+    print ("\nThank you! Here is your Bank info: ")
     print ("Name: {}".format(bank_acc.name.title()))
-    print ("AccNum: {}".format(bank_acc.accnum))
+    print ("Acccount Number: {}".format(bank_acc.accnum))
     print ("Balance: ${:.2f}\n\n".format(int(bank_acc.balance)))
 
 def account_valid(name):
-    if name in accounts.keys():
-        return True
-    else:
-        return False
+    return True if name in accounts.keys() else False
 
 def user_menu():
     return input("What would you like to do? \n"\
@@ -55,7 +57,6 @@ def user_menu():
             "  Enter '3' to deposit money \n"\
             "  Enter '4' to check your balance \n"\
             "  Enter '5' to exit. \n")
-    return user_input
 
 print ("Welcome to Bank of amERICa!")
 
@@ -64,7 +65,7 @@ while True:
     if user_input.lower() == '1':
         print ("Great, lets get some information from you...", end="")
         name = input("What is your full name? ")
-        if name in accounts.keys():
+        if account_valid(name):
             print("You already has an account with us! \n")
             continue
 
@@ -75,11 +76,11 @@ while True:
         name = input("What is the name under the account? ")
         if account_valid(name):
             if user_input.lower() == '2':
-                withdraw_amount = input("How much would you like to withdraw? ")
-                accounts[name].withdraw(int(withdraw_amount))
+                withdraw_amount = int(input("How much would you like to withdraw? "))
+                accounts[name].withdraw(withdraw_amount)
             elif user_input.lower() == '3':
-                deposit_amount = input("How much would you like to deposit? ")
-                accounts[name].deposit(int(deposit_amount))
+                deposit_amount = int(input("How much would you like to deposit? "))
+                accounts[name].deposit(deposit_amount)
             elif user_input.lower() == '4':
                 accounts[name].print_balance()
         else:
