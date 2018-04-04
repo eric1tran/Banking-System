6410b52edea75c370b6fdce197d8b8123e6ab8cf
Eric Tran <eric1tran@gmail.com>
2018-04-04 00:47:10 -0500

    Refactor client/server code pt. 1

--- BankClient.py
+++ BankClient.py
@@ -1,28 +1,75 @@
 import socket
 import sys
+import BankServer
 
+def display_menu():
+    print ("What would you like to do? \n"\
+            "  Enter 'C' to create an account. \n"\
+            "  Enter 'W' to withdraw money \n"\
+            "  Enter 'D' to deposit money \n"\
+            "  Enter 'B' to show your balance \n"\
+            "  Enter 'T' to show your transactions \n"\
+            "  Enter 'E' to exit. \n")
 #Create
 try:
     s = socket.socket()
 except socket.error as err:
     print("Socket creation failed with error {}...".format(err))
 
-# Connect to server
+# Connect to Bank server
 try:
     s.connect(('127.0.0.1', 12345))
 except socket.error as msg:
-    print("Couldn't connect to server: {}", msg)
+    print("Couldn't connect to amERICa Bank server: {}", msg)
     sys.exit(1)
 
-print("Connected to server...")
+print("Connected to server...\n\n")
 
-data_input = input(">> ")
+print("Welcome to Bank of amERICa!...\n")
 
 # Send *encoded* data
-while data_input.lower().strip() != 'quit':
-    s.sendall(data_input.encode())
-    data = s.recv(1024).decode()
-    print("Data echoed from server: {}".format(data))
-    data_input = input(" --> ")
+while True:
+    display_menu()
+    user_input = input(">> ")
+
+    # TODO: Arg parse?
+    if user_input.lower() == 'c':
+        print("Great, lets get some information from you...", end="")
+        name = input("What is your full name? ")
+        if BankServer.validate_account(name):
+            print("You already has an account with us! \n")
+            continue
+
+        initial = input("How much would you like for your initial deposit? ")
+        password = BankServer.request_password()
+
+        BankServer.create_account(name, initial, password)
+
+    elif user_input.lower() in "wdbt":
+        name = input("What is the name under the account? ")
+        if BankServer.validate_account(name):
+            password = input("Enter your password: ")
+            if BankServer.validate_password(name, password):
+                if user_input.lower() == 'w':
+                    withdraw_amount = int(input("How much would you like to withdraw? "))
+                    BankServer.withdraw(name, withdraw_amount)
+                elif user_input.lower() == 'd':
+                    deposit_amount = int(input("How much would you like to deposit? "))
+                    BankServer.deposit(name, deposit_amount)
+                elif user_input.lower() == 'b':
+                    BankServer.show_balance(name)
+                elif user_input.lower() == 't':
+                    BankServer.show_transactions(name)
+        else:
+            print("Sorry, you do not have an account with us. Please create one! \n")
+
+    elif user_input.lower() == 'e':
+        break
+    else:
+        print("Invalid input. Please try again. \n")
+
+    #s.send(user_input.encode())
+    #data_received = s.recv(1024).decode()
+    #print("Data echoed from server: {}".format(data_received))
 
 s.close()
--- BankServer.py
+++ BankServer.py
@@ -1,36 +1,90 @@
 import socket
 import sys
+from BankAccount import BankAccount
 
-print("BankServer")
-
-#Create
-try:
-    s = socket.socket()
-    print("Socket successfully created...")
-except socket.error as err:
-    print("Socket creation failed with error {}...".format(err))
+accounts = {}
 
 
-# Reuse and Bind
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(('', 12345))
+def create_account(user_name, initial_deposit, password):
+    """
+    :param user_name: User name to create a Bank Account for
+    :param initial_deposit: The initial amount user wants to deposit into account
+    :return: None
+    """
+    bank_acc = BankAccount(user_name, initial_deposit, password)
+    accounts[user_name] = bank_acc
 
-# Listen
-s.listen(5)
-print("Socket is listening for incoming clients...")
+    print("\nThank you! Here is your Bank info: ")
+    print("Name: {}".format(bank_acc.name.title()))
+    print("Acccount Number: {}".format(bank_acc.accnum))
+    print("Balance: ${:.2f}\n\n".format(int(bank_acc.balance)))
 
-# Accept connections (blocking)
-client, address = s.accept()
-print("Connection accepted with {}\n".format(address))
 
-# Echo data recieved from clients until 'quit' received
-while True:
-    data = client.recv(1024).decode()
-    if data.strip() == 'quit':
-        print("Closing connection...")
-        client.close()
-        sys.exit(1)
-    elif data:
-        print("Data recieved from client: {}".format(data))
-        client.sendall(data.encode())
+def request_password():
+    password = input("Enter a password for the account: ")
+    confirm_password = input("Re-enter password: ")
+    if password != confirm_password:
+        print("Passwords did not match. Try again")
+        request_password()
 
+    return password
+
+
+def validate_account(name):
+    return True if name in accounts.keys() else False
+
+
+def validate_password(name, password):
+    return accounts[name].validate_password(password)
+
+
+def withdraw(name, amount):
+    accounts[name].withdraw(amount)
+
+
+def deposit(name, amount):
+    accounts[name].deposit(amount)
+
+
+def show_balance(name):
+    accounts[name].show_balance()
+
+
+def show_transactions(name):
+    accounts[name].show_transactions()
+
+
+def main():
+    #Create
+    try:
+        s = socket.socket()
+        print("Socket successfully created...")
+    except socket.error as err:
+        print("Socket creation failed with error {}...".format(err))
+
+
+    # Reuse and Bind
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(('', 12345))
+
+    # Listen
+    s.listen(5)
+    print("Socket is listening for incoming clients...")
+
+    # Accept connections (blocking)
+    client, address = s.accept()
+    print("Connection accepted with {}\n".format(address))
+
+    # Echo data recieved from clients until 'quit' received
+    while True:
+        user_input = client.recv(1024).decode()
+        if not user_input:
+            print("Closing connection...")
+            client.close()
+            sys.exit(1)
+        else:
+            print("Data recieved from client: {}".format(user_input))
+            #client.sendall(str(data).upper().encode())
+
+if __name__ == "__main__":
+    main()
